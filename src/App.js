144d5cb9c508caeb5112f08172c95647f9d9297c
lupusalba <a145296@gmail.com>
2022-08-28 23:36:50 +0200

    added music files, problem with stoping music unsolved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import Axios from 'axios'
 import { useState, useEffect } from 'react';
 import './App.css';
-import SearchIcon from './searchIcon.png'
+
+// music
+import Music1 from './Music/ambient-piano-ampamp-strings-10711.mp3'
+import Music2 from './Music/morning-garden-acoustic-chill-15013.mp3'
+import Music3 from './Music/cinematic-atmosphere-score-2-22136.mp3'
+import Music4 from './Music/relax-in-the-forest-background-music-for-video-9145.mp3'
+import Music5 from './Music/cinematic-documentary-115669.mp3'
+import Music6 from './Music/mindfulness-relaxation-amp-meditation-music-22174.mp3'
 
 function App() {
 
@@ -10,7 +17,7 @@ function App() {
     key: "9342397-9c4d96ed2afeb8a6567bd1d19"
   }
   // search term/s
-  const [search, setSearch] = useState('');
+  // const [search, setSearch] = useState('');
 
   // all photos retrived from pixabay api <=> 20 photos
   const [photos, setPhotos] = useState({})
@@ -21,8 +28,30 @@ function App() {
   // counter
   const [count, setCount] = useState(1)
 
+  // const [musicAuthor, setMusicAuthor] = useState([
+  //   '<a href="https://pixabay.com/users/daddysmusic-22836301/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=10711">Zakhar Valaha</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=10711">Pixabay</a>',
+  //   '<a href="https://pixabay.com/users/olexy-25300778/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=15013">Olexy</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=15013">Pixabay</a>',
+  //   '<a href="https://pixabay.com/users/musictown-25873992/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=22136">Musictown</a> from <a href="https://pixabay.com/music//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=22136">Pixabay</a>',
+  //   '<a href="https://pixabay.com/users/lesfm-22579021/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=9145">Oleksii Kaplunskyi</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=9145">Pixabay</a>',
+  //   '<a href="https://pixabay.com/users/lexin_music-28841948/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=115669">Aleksey Chistilin</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=115669">Pixabay</a>',
+  //   '<a href="https://pixabay.com/users/john_kensy_music-26026995/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=22174">John K.</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=22174">Pixabay</a>'
+  
+  // ])
+
   // theme button
-  const [theme, setTheme] = useState("forest")
+  const [theme, setTheme] = useState({
+    type: "forest",
+    sound: Music1
+  })
+
+
+  const playAudio = (sound) => {
+    new Audio(sound).play()
+  }
+
+  const stopAudio = (sound) => {
+     Audio(sound).pause()
+  }
 
 
 //default theme
@@ -40,18 +69,43 @@ useEffect(() => {
 },[])
 
 
+// const choseAudio = (theme) => {
+//   console.log(theme)
+//   switch(theme) {
+//     case "forest": setAudio(ForestSound); break;
+//     case "river": setAudio(RiverSound); break;
+//     default: setAudio(NoRival); break;
+//   }
+// }
+
+
 // get new theme
 useEffect(() => {
 
   async function fetchPhotos() {
-    const res = await fetch(`${api.base}?key=${api.key}&q=${theme}&image_type=photo`)
+    stopAudio(theme.sound)
+    const res = await fetch(`${api.base}?key=${api.key}&q=${theme.type}&image_type=photo`)
     const data = await res.json()
     setPhotos(data.hits)
     setPhoto(data.hits[0])
+    
   }
   fetchPhotos()
+  playAudio(theme.sound)
 
 },[theme])
+
+
+
+// // set audio
+// useEffect((theme) => {
+//   console.log(theme)
+//   console.log("audioooooo")
+  
+//   choseAudio(theme)
+  
+// }, [audio])
+
 
 const test = {
   backgroundImage: `url(${photo.largeImageURL})`
@@ -66,12 +120,25 @@ const changeBackground = () => {
 
 const handleSearch = (e) => {
   e.preventDefault()
-  setTheme(e.target.id)
+
+
+  switch(e.target.id) {
+    case "mountain": setTheme({type: "mountain", sound: Music1}); break;
+    case "forest": setTheme({type: "forest", sound: Music2}); break;
+    case "river": setTheme({type: "river", sound: Music3}); break;
+    case "night": setTheme({type: "night", sound: Music4}); break;
+    case "nature": setTheme({type: "nature", sound: Music5}); break;
+    case "sea": setTheme({type: "sea", sound: Music6}); break;
+    
+    default: setTheme({type: "forest", sound: Music1}); break;
+  }
+
 }
 
 
 return (
   <div className="App" style={test}>
+
 
 
     <div className="choices">
@@ -83,11 +150,20 @@ return (
       <button onClick={handleSearch} id="sea" className="choiceBtn">sea</button>
     </div>
 
+    <button onClick={stopAudio(theme.sound)}>Stop</button>
+
     <button onClick={changeBackground} className="refresh">Refresh</button>
 
-    <div className="credits">
-      <a className="authorCredits" href={photo.pageURL}>{photo.user}</a>
-      <a className="hostCredits" href="https://www.pexels.com/">Provided by Pexels</a>
+    <div className="creditsWrapper">
+      <div className="credits">
+        <a className="authorCredits" href={photo.pageURL}>Photo Author{photo.user}</a>
+        <a className="hostCredits" href="https://www.pexels.com/">Provided by Pexels</a>
+      </div>
+
+      {/* <div className="credits">
+        <a className="musicAuthorCredits authorCredits" href="#">Music Author{theme.musicAuthor}</a>
+        <a className="hostCredits" href="https://www.pexels.com/">Provided by Pexels</a>
+      </div> */}
     </div>
   </div>
 );
