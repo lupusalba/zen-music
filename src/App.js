df7a3ae9cc332823b51ea54342d26dff9b67deb1
lupusalba <a145296@gmail.com>
2022-08-29 19:32:52 +0200

    small changes css

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
   //   '<a href="https://pixabay.com/users/lesfm-22579021/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=9145">Oleksii Kaplunskyi</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=9145">Pixabay</a>',
   //   '<a href="https://pixabay.com/users/lexin_music-28841948/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=115669">Aleksey Chistilin</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=115669">Pixabay</a>',
   //   '<a href="https://pixabay.com/users/john_kensy_music-26026995/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=22174">John K.</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=22174">Pixabay</a>'
-  
+
   // ])
 
   // theme button
@@ -51,138 +51,132 @@ function App() {
   })
 
 
-//default theme
-useEffect(() => {
-  let term = "forest"
+  //default theme
+  useEffect(() => {
+    let term = "forest"
 
-  async function fetchPhotos() {
-    const res = await fetch(`${api.base}?key=${api.key}&q=${term}&image_type=photo`)
-    const data = await res.json()
-    setPhotos(data.hits)
-    setPhoto(data.hits[0])
+    async function fetchPhotos() {
+      const res = await fetch(`${api.base}?key=${api.key}&q=${term}&image_type=photo`)
+      const data = await res.json()
+      setPhotos(data.hits)
+      setPhoto(data.hits[0])
+    }
+    fetchPhotos()
+  }, [])
+
+
+
+
+  // get new theme
+  useEffect(() => {
+    async function fetchPhotos() {
+      //stopMusic()
+      const res = await fetch(`${api.base}?key=${api.key}&q=${theme.type}&image_type=photo`)
+      const data = await res.json()
+      setPhotos(data.hits)
+      setPhoto(data.hits[0])
+
+    }
+    fetchPhotos()
+    console.log(theme.sound)
+    let s = theme.sound
+    let audio = document.getElementById('audio');
+    audio.pause();
+    audio.setAttribute('src', s);
+    audio.load();
+    audio.play();
+
+  }, [theme])
+
+
+
+
+
+  const test = {
+    backgroundImage: `url(${photo.largeImageURL})`
   }
-  fetchPhotos()
 
-},[])
-
-
-
-
-// get new theme
-useEffect(() => {
-  //stopMusic()
-
-
-
-  async function fetchPhotos() {
-    //stopMusic()
-    const res = await fetch(`${api.base}?key=${api.key}&q=${theme.type}&image_type=photo`)
-    const data = await res.json()
-    setPhotos(data.hits)
-    setPhoto(data.hits[0])
-    
+  const changeBackground = () => {
+    console.log("clicked")
+    console.log(`${photos[count].largeImageURL}`)
+    setPhoto(photos[count])
+    setCount(count + 1)
   }
-  fetchPhotos()
-  console.log(theme.sound)
-  // let audioElement = document.getElementById("audio")
-  // let source = document.getElementById("audioSource")
-  // source.src = theme.sound
-  // console.log(source)
-  let s = theme.sound
-  let audio = document.getElementById('audio');
-  audio.pause();
-  audio.setAttribute('src', s);
-  audio.load();
-  audio.play();
 
-},[theme])
+  const handleSearch = (e) => {
+    e.preventDefault()
+
+    let audioSource = document.getElementById('audio');
+    audioSource.pause()
 
 
+    switch (e.target.id) {
+      case "mountain": setTheme({ type: "mountain", sound: Music1 }); break;
+      case "forest": setTheme({ type: "forest", sound: Music2 }); break;
+      case "river": setTheme({ type: "river", sound: Music3 }); break;
+      case "night": setTheme({ type: "night", sound: Music4 }); break;
+      case "nature": setTheme({ type: "nature", sound: Music5 }); break;
+      case "sea": setTheme({ type: "sea", sound: Music6 }); break;
 
+      default: setTheme({ type: "forest", sound: Music3 }); break;
+    }
 
-
-const test = {
-  backgroundImage: `url(${photo.largeImageURL})`
-}
-
-const changeBackground = () => {
-  console.log("clicked")
-  console.log(`${photos[count].largeImageURL}`)
-  setPhoto(photos[count])
-  setCount(count + 1)
-}
-
-const handleSearch = (e) => {
-  e.preventDefault()
-
-  let audioSource = document.getElementById('audio');
-  audioSource.pause()
-
-
-  switch(e.target.id) {
-    case "mountain": setTheme({type: "mountain", sound: Music1}); break;
-    case "forest": setTheme({type: "forest", sound: Music2}); break;
-    case "river": setTheme({type: "river", sound: Music3}); break;
-    case "night": setTheme({type: "night", sound: Music4}); break;
-    case "nature": setTheme({type: "nature", sound: Music5}); break;
-    case "sea": setTheme({type: "sea", sound: Music6}); break;
-    
-    default: setTheme({type: "forest", sound: Music3}); break;
   }
-  
-}
 
-const startMusic = () => {
-  let audioSource = document.getElementById('audio');
-  console.log(audioSource)
-  audioSource.play()
-}
+  const startMusic = () => {
+    let audioSource = document.getElementById('audio');
+    console.log(audioSource)
+    audioSource.play()
+  }
 
-const stopMusic = () => {
-  let audioSource = document.getElementById('audio');
-  audioSource.pause()
-}
+  const stopMusic = () => {
+    let audioSource = document.getElementById('audio');
+    audioSource.pause()
+  }
 
 
 
 
-return (
-  <div className="App" style={test}>
+  return (
+    <div className="App" style={test}>
 
 
 
-    <div className="choices">
-      <button onClick={handleSearch} id="mountain" className="choiceBtn">mountain</button>
-      <button onClick={handleSearch} id="forest" className="choiceBtn">forest</button>
-      <button onClick={handleSearch} id="river" className="choiceBtn">river</button>
-      <button onClick={handleSearch} id="night" className="choiceBtn">night</button>
-      <button onClick={handleSearch} id="nature" className="choiceBtn">nature</button>
-      <button onClick={handleSearch} id="sea" className="choiceBtn">sea</button>
-    </div>
-
-    <button onClick={stopMusic}>stop</button>
-
-    <button onClick={startMusic}>start</button>
-
-    <audio id="audio">
-      <source id="audioSource" src={Music1} />
-    </audio>
-
-    <button onClick={changeBackground} className="refresh">Refresh</button>
-
-    <div className="creditsWrapper">
-      <div className="credits">
-        <a className="authorCredits" href={photo.pageURL}>Photo Author{photo.user}</a>
-        <a className="hostCredits" href="https://www.pexels.com/">Provided by Pexels</a>
+      <div className="choices">
+        <button onClick={handleSearch} id="mountain" className="choiceBtn">mountain</button>
+        <button onClick={handleSearch} id="forest" className="choiceBtn">forest</button>
+        <button onClick={handleSearch} id="river" className="choiceBtn">river</button>
+        <button onClick={handleSearch} id="night" className="choiceBtn">night</button>
+        <button onClick={handleSearch} id="nature" className="choiceBtn">nature</button>
+        <button onClick={handleSearch} id="sea" className="choiceBtn">sea</button>
       </div>
 
-      {/* <div className="credits">
+
+      <div className="stopStart">
+        <button onClick={stopMusic}>stop</button>
+
+        <button onClick={startMusic}>start</button>
+      </div>
+
+      <audio id="audio">
+        <source id="audioSource" src={Music1} />
+      </audio>
+
+      <button onClick={changeBackground} className="refresh">Refresh</button>
+
+      <div className="creditsWrapper">
+        <div className="credits">
+          <a className="authorCredits" href={photo.pageURL}>Photo Author{photo.user}</a>
+          <a className="hostCredits" href="https://www.pexels.com/">Provided by Pexels</a>
+        </div>
+
+        {/* <div className="credits">
         <a className="musicAuthorCredits authorCredits" href="#">Music Author{theme.musicAuthor}</a>
         <a className="hostCredits" href="https://www.pexels.com/">Provided by Pexels</a>
       </div> */}
+      </div>
     </div>
-  </div>
-);
+  );
 }
 
 export default App;
